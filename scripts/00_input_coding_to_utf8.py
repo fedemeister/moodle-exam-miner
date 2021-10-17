9907867c20c1de264649fe6ea4b732a7715eeb6a
Federico Carrillo Chaves <fedemeister@outlook.es>
2021-10-17 18:34:56 +0200

    feat(scripts): :sparkles: make input files to utf8 encode

---
+++ scripts/00_input_coding_to_utf8.py
@@ -0,0 +1,20 @@
+import io
+import json
+import codecs
+
+with io.open('files/tool_input/exam_califications.json', 'r', encoding='utf-8-sig') as json_file:  
+    data = json.load(json_file)
+with codecs.open('files/tool_input/exam_califications_utf8.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+with io.open('files/tool_input/exam_logs.json', 'r', encoding='utf-8-sig') as json_file:  
+    data = json.load(json_file)    
+with codecs.open('files/tool_input/exam_logs_utf8.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
+    
+
+with io.open('files/tool_input/exam_answers.json', 'r', encoding='utf-8-sig') as json_file:  
+    data = json.load(json_file)    
+with codecs.open('files/tool_input/exam_answers_utf8.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
