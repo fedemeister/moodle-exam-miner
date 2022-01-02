184c0523072632e8bbf85f60faa2e6cb01ea7aae
Federico Carrillo Chaves <fedemeister@outlook.es>
2022-01-02 19:49:19 +0100

    fix: :rocket: modify requirements.txt to deploy in heroku

--- run_plotly.py
+++ run_plotly.py
@@ -1,6 +1,3 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 from dash import dcc
 from dash import html
@@ -12,7 +9,7 @@ import web.graph_scatter_plot
 import web.graph_fig_estudiantes
 
 app = dash.Dash(__name__)
-
+server = app.server
 
 nube_puntos = web.graph_scatter_plot.nube_puntos()
 
