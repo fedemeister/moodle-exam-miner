7052e7a94fb040ed6918fa2bc4dc9aece47be075
fedemeister <fedemeister@outlook.es>
2022-01-31 21:59:50 +0100

    refactor(tests): change the name of run_script04 to get_answer_times_df in file test_answer_times.py

--- tests/test_answer_times.py
+++ tests/test_answer_times.py
@@ -4,13 +4,13 @@ from tests.fixtures_global.fixtures_global_anonimizados import fixture_df_answer
 from tests.fixtures_global.fixtures_global_anonimizados import fixture_new_json_logs_anonymized
 from tests.fixtures_global.fixtures_global_anonimizados import fixture_json_marks_formatted_anonymized
 
-from scripts.script4.answer_times import run_script04
+from scripts.script4.answer_times import get_answer_times_df
 
 
 def test_answer_times_df(fixture_json_marks_formatted_anonymized, fixture_new_json_logs_anonymized,
                          fixture_df_answer_times_anonymized):
     marks_df = fixture_json_marks_formatted_anonymized
     json = fixture_new_json_logs_anonymized
-    actual = run_script04(marks_df, json)
+    actual = get_answer_times_df(marks_df, json)
     expected = fixture_df_answer_times_anonymized
     pd.testing.assert_frame_equal(actual, expected)
