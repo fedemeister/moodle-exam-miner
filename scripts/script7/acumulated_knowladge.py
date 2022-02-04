655af45d1e01ca4951956e8bc13b81c4cae8de08
fedemeister <fedemeister@outlook.es>
2022-02-04 18:22:17 +0100

    feat(scripts): :sparkles: add some refactor, docstring and now the script returns 4 very important values (merge_df, ratio_preguntas, conocimiento_acumulado, merge_df_json)

--- scripts/script7/acumulated_knowladge.py
+++ scripts/script7/acumulated_knowladge.py
@@ -5,7 +5,19 @@ from datetime import datetime
 from scripts.script6.py_collaborator_outputs import NumpyEncoder
 
 
-def merge_dataframes(py_collaboartor_df, respuestas_df, preguntas_df, hora_respuestas_df):
+def merge_dataframes(py_collaboartor_df: pd.DataFrame, respuestas_df: pd.DataFrame, preguntas_df: pd.DataFrame,
+                     hora_respuestas_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Esta función une cada uno de los dataframes de entrada en un único dataframe
+    Args:
+        py_collaboartor_df: Dataframe pycollaborator.
+        respuestas_df: Dataframe de las respuestas de los estudiantes durante el examen.
+        preguntas_df: Dataframe sacado del XML con el conjunto global de preguntas ya limpio.
+        hora_respuestas_df: Dataframe con las horas de cada respuesta para cada estudiante.
+
+    Returns:
+        Un dataframe "merge_df" que tiene todos la información unida.
+    """
     merge_df = pd.DataFrame(data=py_collaboartor_df[['Nombre', 'Código', 'Tiempo',
                                                      'Inicio', 'Fin', 'Segundos', 'Nota', 'Productividad']]
                             , columns=['Nombre', 'Código', 'Tiempo',
@@ -35,20 +47,33 @@ def merge_dataframes(py_collaboartor_df, respuestas_df, preguntas_df, hora_respu
                 merge_df['Q' + str(x) + '_t'][i] = time
                 merge_df['Q' + str(x) + '_a'][i] = aux
                 merge_df['Q' + str(x) + '_q'][i] = preguntas_df['Question'][preguntas_df['Answer'] == aux].values[0]
-                merge_df['Q' + str(x) + '_m'][i] = preguntas_df['Qualification'][preguntas_df['Answer'] == aux].values[
-                    0]
+                merge_df['Q' + str(x) + '_m'][i] = preguntas_df['Mark'][preguntas_df['Answer'] == aux].values[0]
             else:
                 merge_df['Q' + str(x) + '_t'][i] = time
                 merge_df['Q' + str(x) + '_a'][i] = aux
                 merge_df['Q' + str(x) + '_q'][i] = 'Desconocida'
                 merge_df['Q' + str(x) + '_m'][i] = np.float64(0)
-
     merge_df = merge_df.set_index('Nombre')
     merge_df.to_excel('files/tool_output/07_acumulated_knowladge/merge_df.xlsx')
     return merge_df
 
 
-def misma_pregunta_luego(x, pregunta, respuesta, hora_respuesta, merge_df):
+def misma_pregunta_luego(x, pregunta: str, respuesta: str, hora_respuesta, merge_df: pd.DataFrame) -> (bool, str):
+    """
+    Devuelve el siguiente alumno que ha respondido correctamente a la misma pregunta que el estudiante_sub_i.
+    Esta función sirve para la función CA (Conocimiento Acumulado) que mide una posible cadena de colaboración
+    entre los estudiantes encadenando preguntas correctamente respondidas.
+    Args:
+        x: Número de la pregunta (1 a la 10).
+        pregunta: Pregunta que estamos mirando (depende del valor de X que marca qué orden de pregunta fue).
+        respuesta: Respuesta que se utilizó.
+        hora_respuesta: Hora en la que se respondió.
+        merge_df: Dataframe con todos los datos que vamos a consultar para acceder únicamente a un dataframe.
+
+    Returns:
+        Código del estudiante que respondió igual que el estudiante_sub_i la siguiente vez que esa pregunta apareció
+        o puede devolver 0, False si no encuentra ese estudiante.
+    """
     if len(merge_df['Código'][
                (merge_df['Q' + str(x) + '_q'] == pregunta) & (merge_df['Q' + str(x) + '_t'] > hora_respuesta)]) > 0:
         cod1 = merge_df['Código'][
@@ -67,7 +92,20 @@ def misma_pregunta_luego(x, pregunta, respuesta, hora_respuesta, merge_df):
         return False, '0'
 
 
-def CA(i, merge_df):
+def CA(i: int, merge_df: pd.DataFrame) -> {}:
+    """
+    CA (Conocimiento Acumulado) es una función que mide una posible red sospechosa de intercambio de preguntas
+    observando si se producen cadenas de estudiantes que van respondiendo la misma pregunta de forma seguida
+    de manera correcta.
+    Args:
+        i: estudiante_sub_i
+        merge_df: Dataframe con todos los datos que vamos a consultar para acceder únicamente a un dataframe.
+
+    Returns:
+        El conocimiento acumulado del estudiante (preguntas que el siguiente estudiante que la tuvo, respondió
+        también correctamente)
+
+    """
     student = {}
     codigo = merge_df['Código'][i]
     inicio = merge_df['Inicio'][i]
@@ -87,7 +125,7 @@ def CA(i, merge_df):
 
         if puntuacion == 1.0:
             flag, cod = misma_pregunta_luego(x, pregunta, respuesta, hora, merge_df)
-            if flag == True:
+            if flag:
                 lista_CA_r.append(respuesta)
                 lista_CA_i.append(x)
                 lista_CA_p.append(pregunta)
@@ -107,7 +145,18 @@ def CA(i, merge_df):
     return student
 
 
-def ratio_preg(i, preguntas_df, merge_df, verbose=True):
+def ratio_preg(i, preguntas_df, merge_df, verbose=True) -> {}:
+    """
+    Ratio de aciertos y fallos para cada pregunta
+    Args:
+        i: pregunta_sub_i (pregunta que toca averiguar)
+        preguntas_df: Dataframe sacado del XML con el conjunto global de preguntas ya limpio.
+        merge_df: Dataframe con todos los datos que vamos a consultar para acceder únicamente a un dataframe.
+        verbose: True por defecto para devolver la mayor cantidad de detalle para la salida.
+
+    Returns:
+
+    """
     questions = {}
     preg = preguntas_df['Question'].iloc[i]
     lista_c = []
@@ -147,37 +196,56 @@ def ratio_preg(i, preguntas_df, merge_df, verbose=True):
     return questions
 
 
-def run_script07():
-    calificaciones_df = pd.read_excel('files/tool_output/03_anwers_and_califications_dataframe/marks.xlsx')
-    respuestas_df = pd.read_excel('files/tool_output/05_answers_and_questions_cleaned/answers_cleaned.xlsx')
-    preguntas_df = pd.read_excel('files/tool_output/05_answers_and_questions_cleaned/all_questions.xlsx')
-    hora_respuestas_df = pd.read_excel('files/tool_output/04_answers_time/answer_times_merged.xlsx')
-    py_collaboartor_df = pd.read_excel('files/tool_output/06_py_collaborator_outputs/py_cheat_df.xlsx')
-    # py_collaboartor_df = respuestas_df[['Nombre', 'Código', 'Inicio', 'Fin', 'Segundos', 'Nota']]
-    # py_collaboartor_df["Productividad"] = (py_collaboartor_df["Nota"] / (py_collaboartor_df["Segundos"] / 60))
-
-    merge_df = merge_dataframes(py_collaboartor_df, respuestas_df, preguntas_df, hora_respuestas_df)
+def run_script07(answers_df_cleaned, df_xml_cleaned, answer_times_merged_df, py_cheat_df):
+    merge_df = merge_dataframes(py_cheat_df, answers_df_cleaned, df_xml_cleaned, answer_times_merged_df)
 
     salida_preg = []
-    for i in range(0, len(preguntas_df), 4):
-        salida_preg.append(ratio_preg(i, preguntas_df, merge_df, verbose=False))
+    for i in range(0, len(df_xml_cleaned), 4):
+        salida_preg.append(ratio_preg(i, df_xml_cleaned, merge_df, verbose=False))
 
-    diccionario = {"questions": salida_preg}
+    ratio_preguntas = {"questions": salida_preg}
 
     with open('files/tool_output/07_acumulated_knowladge/outputRatioPreg.json', 'w', encoding='utf8') as outfile:
-        json.dump(diccionario, outfile, indent=2, cls=NumpyEncoder, ensure_ascii=False)
+        json.dump(ratio_preguntas, outfile, indent=2, cls=NumpyEncoder, ensure_ascii=False)
 
     salida = []
     for i in range(len(merge_df)):
         salida.append(CA(i, merge_df))
 
-    diccionario = {"students": salida}
+    conocimiento_acumulado = {"students": salida}
 
     with open('files/tool_output/07_acumulated_knowladge/outputCA.json', 'w', encoding='utf8') as outfile:
-        json.dump(diccionario, outfile, indent=2, cls=NumpyEncoder, ensure_ascii=False)
+        json.dump(conocimiento_acumulado, outfile, indent=2, cls=NumpyEncoder, ensure_ascii=False)
 
     result = merge_df.to_json(index='Nombre', orient="index", date_format='iso', date_unit='s')
-    parsed = json.loads(result)
+    merge_df_json = json.loads(result)
 
     with open('files/tool_output/07_acumulated_knowladge/merge_df.json', 'w', encoding='utf8') as outfile:
-        json.dump(parsed, outfile, indent=2, cls=NumpyEncoder, ensure_ascii=False)
+        json.dump(merge_df_json, outfile, indent=2, cls=NumpyEncoder, ensure_ascii=False)
+
+    merge_df = merge_df_columns_to_datetime64(merge_df)
+
+    return merge_df, ratio_preguntas, conocimiento_acumulado, merge_df_json
+
+
+def merge_df_columns_to_datetime64(merge_df):
+    """
+
+    Args:
+        merge_df:
+
+    Returns:
+
+    """
+    merge_df['Q0_t'] = pd.to_datetime(merge_df['Q0_t'])
+    merge_df['Q1_t'] = pd.to_datetime(merge_df['Q1_t'])
+    merge_df['Q2_t'] = pd.to_datetime(merge_df['Q2_t'])
+    merge_df['Q3_t'] = pd.to_datetime(merge_df['Q3_t'])
+    merge_df['Q4_t'] = pd.to_datetime(merge_df['Q4_t'])
+    merge_df['Q5_t'] = pd.to_datetime(merge_df['Q5_t'])
+    merge_df['Q6_t'] = pd.to_datetime(merge_df['Q6_t'])
+    merge_df['Q7_t'] = pd.to_datetime(merge_df['Q7_t'])
+    merge_df['Q8_t'] = pd.to_datetime(merge_df['Q8_t'])
+    merge_df['Q9_t'] = pd.to_datetime(merge_df['Q9_t'])
+    merge_df['Q10_t'] = pd.to_datetime(merge_df['Q10_t'])
+    return merge_df
