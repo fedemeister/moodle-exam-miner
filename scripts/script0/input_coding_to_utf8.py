8c9f6fd86fe44672f1c08278a8010f2751f4fbf5
fedemeister <fedemeister@outlook.es>
2022-02-06 23:11:28 +0100

    refactor(scripts): :gem: add docstring and refactoring of names

--- scripts/script0/input_coding_to_utf8.py
+++ scripts/script0/input_coding_to_utf8.py
@@ -1,20 +1,28 @@
 import io
 import json
 import codecs
+from typing import Tuple, List
 
 
-def run_script00():
-    with io.open('files/tool_input/exam_califications.json', 'r', encoding='utf-8-sig') as json_file:
-        data = json.load(json_file)
+def jsons_to_utf8() -> Tuple[List, List, List]:
+    """
+    Carga los 3 json (exam_califications, exam_logs y exam_answers) contenidos en /tool_input/ y los pasa a utf8
+    Returns:
+        Los 3 json en utf8
+    """
+    with io.open('files/tool_input/exam_califications.json', 'r', encoding='utf-8-sig') as json_marks:
+        json_marks = json.load(json_marks)
     with codecs.open('files/tool_input/exam_califications_utf8.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+        json.dump(json_marks, f, ensure_ascii=False, indent=2)
 
-    with io.open('files/tool_input/exam_logs.json', 'r', encoding='utf-8-sig') as json_file:
-        data = json.load(json_file)
+    with io.open('files/tool_input/exam_logs.json', 'r', encoding='utf-8-sig') as json_logs:
+        json_logs = json.load(json_logs)
     with codecs.open('files/tool_input/exam_logs_utf8.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+        json.dump(json_logs, f, ensure_ascii=False, indent=2)
 
-    with io.open('files/tool_input/exam_answers.json', 'r', encoding='utf-8-sig') as json_file:
-        data = json.load(json_file)
+    with io.open('files/tool_input/exam_answers.json', 'r', encoding='utf-8-sig') as json_answers:
+        json_answers = json.load(json_answers)
     with codecs.open('files/tool_input/exam_answers_utf8.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+        json.dump(json_answers, f, ensure_ascii=False, indent=2)
+
+    return json_marks, json_logs, json_answers
